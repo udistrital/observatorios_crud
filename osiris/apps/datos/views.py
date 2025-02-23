d22a294116a7ae39c9a0371db0455c2e715c900b
Wilmer Sair Ramos <wilmerramos@useit.co>
2025-02-22 20:46:59 -0500

    docs: Modelos y documentacion de la vista de datos

--- osiris/apps/datos/views.py
+++ osiris/apps/datos/views.py
@@ -3,14 +3,43 @@ from apps.elasticsearch_utils.views import ElasticsearchViewSet
 from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 class DatosViewSet(ElasticsearchViewSet):
 
     def initial(self, request, *args, **kwargs):
         super().initial(request, *args, **kwargs)
         self._nombre_indice = kwargs.get("estructura_id").lower()
 
+    @swagger_auto_schema(
+        operation_description="Crea una nueva estructura de campos",
+        responses={
+            400: openapi.Response(description="Solicitud inválida"),
+        },
+        tags=["Datos"]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_description="Crea una nueva estructura de campos",
+        responses={
+            400: openapi.Response(description="Solicitud inválida"),
+        },
+        tags=["Datos"]
+    )
+    def destroy(self, request, pk=None, *args, **kwargs):
+        return super().destroy(request, pk, *args, **kwargs)
 
-    # Método para obtener un detalle de un objeto por su ID en Elasticsearch
+    @swagger_auto_schema(
+        operation_description="Crea una nueva estructura de campos",
+        responses={
+            400: openapi.Response(description="Solicitud inválida"),
+        },
+        tags=["Datos"]
+    )
     def retrieve(self, request, pk=None, *args, **kwargs):
         cliente = self.get_elasticsearch_client()
         
@@ -20,7 +49,13 @@ class DatosViewSet(ElasticsearchViewSet):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
 
-    # Método para crear un nuevo objeto en Elasticsearch
+    @swagger_auto_schema(
+        operation_description="Crea una nueva estructura de campos",
+        responses={
+            400: openapi.Response(description="Solicitud inválida"),
+        },
+        tags=["Datos"]
+    )
     def create(self, request, *args, **kwargs):
         cliente = self.get_elasticsearch_client()
         datos = request.data
@@ -31,7 +66,13 @@ class DatosViewSet(ElasticsearchViewSet):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    # Método para actualizar un documento en Elasticsearch
+    @swagger_auto_schema(
+        operation_description="Crea una nueva estructura de campos",
+        responses={
+            400: openapi.Response(description="Solicitud inválida"),
+        },
+        tags=["Datos"]
+    )
     def update(self, request, pk=None, *args, **kwargs):
         cliente = self.get_elasticsearch_client()
         datos = request.data
