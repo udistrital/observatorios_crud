abc423432aad5b78754906c9f9c5592aaad0d63f
JulianAvella <julianavella01@gmail.com>
2025-11-21 15:27:35 -0500

    feat: Se quita el panel admin de django para solucionar vulnerabilidad (udistrital/observatorios_cliente#85)

--- osiris/osiris/urls.py
+++ osiris/osiris/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -18,8 +17,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
     ### Documentación ###
     path("documentacion/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("documentacion/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
