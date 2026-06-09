3adbb22d894cbbb465817c8fb195532f2d49234b
JulianAvella <julianavella01@gmail.com>
2026-06-09 11:46:02 -0500

    feat: Se agrega el host del api para que django pueda llamar a los componentes (udistrital/observatorios_crud#70)

--- osiris/osiris/settings.py
+++ osiris/osiris/settings.py
@@ -54,7 +54,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-insecure-key-change-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = ["*"]
 #ALLOWED_HOSTS = [
 #    "pruebasapi.intranetoas.udistrital.edu.co",
 #    "pruebasatlas.portaloas.udistrital.edu.co",
